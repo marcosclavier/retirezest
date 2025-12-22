1683f2a45af28bba3c5076d2d419fa4161e9f88e
marcosclavier <marcosclavier@users.noreply.github.com>
2025-12-22 14:31:56 -0700

    Revert "Add Safari polyfill to fix toLocaleString errors globally"
    This reverts commit c1ba0ddca2383d13e8d673fd38e47d9d0d9cd648.

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import "./safari-polyfill";
 
 // Optimized font loading with next/font
 const inter = Inter({
